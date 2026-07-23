59ebc7b269d9008b2ce63a9ac8b323a6d43c7453
md-4n4s <m.d.4n4s@gmail.com>
2026-07-23 22:16:21 +0500

    Added headers and timeout, and generated a csv file to store scrapped data.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://spice-bite-fast-food.vercel.app/"
 
-response = requests.get(url)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url,headers=headers,timeout=10)
 
 menu = {}
 
@@ -11,13 +16,20 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
 
     products = soup.find_all("article", class_="menu-item")
-    for product in products:
-        name = product.find("button", class_="add-btn")["data-name"]
-        price = product.find("button", class_="add-btn")["data-price"]
-        menu[name] = price
 
-    for key, value in menu.items():
-        print(f"{key}: {value}")
+    with open('menu.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Food","Price"])
+
+        for product in products:
+            name = product.find("button", class_="add-btn")["data-name"].strip()
+            price = product.find("button", class_="add-btn")["data-price"]
+            menu[name] = price
+
+            writer.writerow([name, price])
+
+        for key, value in menu.items():
+            print(f"{key}: {value}")
 
 else:
     print("Error")
